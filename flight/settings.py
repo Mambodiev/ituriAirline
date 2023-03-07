d2b463a1348701e83cb54a92f6a3703045345a0a
diem <mambodieu@gmail.com>
2023-03-07 20:42:52 +0300

    change in template strucure

--- flight/settings.py
+++ flight/settings.py
@@ -2,7 +2,6 @@ import os
 
 from pathlib import Path
 
-SECRET_KEY = False
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-#7o@f2s_*xtknd&kjj2d%b#80x^**=p!3x_z@rmienx!)twj1o'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'flight.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['flight/templates'],                            
+        'DIRS': [os.path.join(BASE_DIR, "templates")],                          
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
